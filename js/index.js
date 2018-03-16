a638ac74ee46ace38a22090d5181855a2751e4f8
Aaron <aachansy2007@gmail.com>
2018-03-15 23:37:58 -0700

    Update to the latest version.
    See src for changes.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// TODO: Make a cool fade in background like in kovenwei.com
+var box = document.getElementsByClassName('box');
+
+var boxOptions = {
+    targets: box,
+    rotate: 360,
+    duration: function(target, i) {
+      return 1000 * (i + 1);
+    },
+    opacity: 1,
+    easing: 'easeInOutCubic',
+    loop: true,
+    direction: 'alternate',
+}
+
+var boxAnimate = anime(boxOptions);
+
+/*
+box.addEventListener('click', function() {
+})
+*/
